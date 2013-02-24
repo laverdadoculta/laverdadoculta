856b6ee55e537f295adb77a63d95ee98e8593cdb
laverdadoculta2@gmail.com <laverdadoculta2@gmail.com@42a75ed2-73e2-e11f-870a-b0c8d9bf7795>
2013-02-24 19:42:12 +0000

    archivo js

--- jsCommentPages.js
+++ jsCommentPages.js
@@ -55,6 +55,6 @@ var jsCommentPages = function(){
 		  	$activePage = $("#" + $activeTab.attr("id") + "-page");
 		  	$activePage.show();
 		};
-	$("head").append("<link id='js-comments-pages-styles' rel='stylesheet' type='text/css' href='http://laverdadoculta.googlecode.com/svn/jsCommentPages.css'/>");
+	$("head").append("<link id='js-comments-pages-styles' rel='stylesheet' type='text/css' href='http://jsblogstop.googlecode.com/svn/commentpages/jsCommentPages.css'/>");
   	$("document").ready(init);
 }();

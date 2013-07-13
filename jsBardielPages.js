7c8f7045a895d4267e4b8af053bcba5a21b644d7
laverdadoculta2@gmail.com <laverdadoculta2@gmail.com@42a75ed2-73e2-e11f-870a-b0c8d9bf7795>
2013-07-13 01:42:33 +0000

    asdfasdf

--- jsBardielPages.js
+++ jsBardielPages.js
@@ -55,6 +55,6 @@ var jsCommentPages = function(){
 		  	$activePage = $("#" + $activeTab.attr("id") + "-page");
 		  	$activePage.show();
 		};
-	$('head').append('<'+'link rel="stylesheet" href="http://jsblogstop.googlecode.com/svn/commentpages/jsCommentPages.css" type="text/css" id="js-comments-pages-styles"/'+'>');
+	$("head").append("<link id='js-comments-pages-styles' rel='stylesheet' type='text/css' href='http://jsblogstop.googlecode.com/svn/commentpages/jsCommentPages.css'/>");
   	$("document").ready(init);
 }();
